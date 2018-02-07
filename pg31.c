2d3ac4361ab5b84a7cec72c2e9875de8f2d74b1b
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-07 16:54:28 +0530

    code
    prgm

---
+++ pg31.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char ch[100];
+printf("\n enter the string:");
+gets(ch);
+printf("\n thr num of characters present in the string is %d",strlen(ch));
+getch();
+}
